9c10dc7d2c0b16ad6974078277125f8c85246837
melihcan sağlam <melihcan_SA@hotmail.com>
2021-02-20 10:38:35 +0300

    newversion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from "ngx-spinner";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private toastr: ToastrService,private spinner: NgxSpinnerService) {}
   title = 'angulartest';
+  ngOnInit(): void {
+   this.showSuccess();
+   this.showSpinner();
+  }
+
+  showSuccess() {
+    this.toastr.success("Melihin yerine hoş geldin", 'Toastr fun!');
+  }
+showSpinner(){
+   /** spinner starts on init */
+   this.spinner.show();
+
+   setTimeout(() => {
+     /** spinner ends after 5 seconds */
+     this.spinner.hide();
+   }, 5000);
+ }
+
+
 }
